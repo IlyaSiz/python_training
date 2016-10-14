43a83f72f8410c489ce533898457039802b9b77e
IlyaSiz <ilyasiz@gmail.com>
2016-10-14 17:10:26 +0300

    Фикстура вынесена в conftest

--- test/tests.py
+++ test/tests.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
 from model.coupon import Coupon
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-def test(app):
+def test_add_and_del_coupon(app):
     app.session.login(email="demo@open-eshop.com", password="demo")
     app.coupon.add_new(Coupon(coupon_name="New great coupon", date="2016-09-15", number_of_coupons="5"))
     app.coupon.find_and_delete("New great coupon")
